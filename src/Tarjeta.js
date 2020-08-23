8efc1447754fbb15009f373b219c6e99d1c305a9
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-08-22 23:15:45 -0500

    make it better

---
+++ src/Tarjeta.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tarjeta = ()=> {
+return (
+<div>
+    <div class="product"></div>
+    <h2 className="product-description">
+        Mi primera tarjeta
+    </h2>
+    <p className="precio">299</p>
+</div>
+);
+}
+
+export default Tarjeta;
