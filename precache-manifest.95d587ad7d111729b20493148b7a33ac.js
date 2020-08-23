c66509568dbdd3a60867541285245128212a4625
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-08-23 00:31:35 -0500

    Updates

---
+++ precache-manifest.95d587ad7d111729b20493148b7a33ac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0038ab54eca97c0d5a87ede29b3d3f5f",
+    "url": "/workshop-react-piano/index.html"
+  },
+  {
+    "revision": "19bdd56a92a3630ea64d",
+    "url": "/workshop-react-piano/static/css/main.612fbc9d.chunk.css"
+  },
+  {
+    "revision": "27c2650df846a92e020f",
+    "url": "/workshop-react-piano/static/js/2.be42e844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/workshop-react-piano/static/js/2.be42e844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19bdd56a92a3630ea64d",
+    "url": "/workshop-react-piano/static/js/main.adabb5e9.chunk.js"
+  },
+  {
+    "revision": "d632f64a81071cddd491",
+    "url": "/workshop-react-piano/static/js/runtime-main.47649b2f.js"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/workshop-react-piano/static/media/do.18fd3f6f.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/workshop-react-piano/static/media/fa.a787534a.wav"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/workshop-react-piano/static/media/la.10af03f3.wav"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/workshop-react-piano/static/media/mi.1f26c778.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/workshop-react-piano/static/media/re.217b943a.wav"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/workshop-react-piano/static/media/si.70924435.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/workshop-react-piano/static/media/sol.785a33d2.wav"
+  }
+]);
